bca61d9119c851b7d30b3e36bfe243f8ce713fe8
Shivam Kumar Nayak <snayak@datopic.com>
2023-10-23 16:38:47 +0530

    widget testing code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,54 @@
-class Main{
+// ##################### Test code for Method :----
 
-  int value = 0;
+// class Main{
 
-  void increment() => value++;
+//   int value = 0;
 
-  void decrement() => value--;
+//   void increment() => value++;
 
-  void incrementPlus2() => value+=2;
+//   void decrement() => value--;
+
+//   void incrementPlus2() => value+=2;
+// }
+
+
+// ##################### Test code for Widget :----
+
+import 'package:flutter/material.dart';
+
+void main(){runApp(MyApp());}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Widget Testing"),
+        ),
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:  [
+         const TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Enter Text"),
+          ),
+
+        TextButton(onPressed: (){}, child: const Text(""))
+      ],
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,33 +1,74 @@
+// ##################### Test code for Method :----
+
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:unit_test_code/main.dart';
+
+// void main() {
+
+//   late Main main;
+
+//   setUp((){
+//     main = Main();
+//   });
+
+//   group("Main Screen",(){
+
+//     test('Value needs to be incremented',() {
+//       main.increment();
+//       expect(main.value, 1);
+//     });
+
+//     test('Value needs to be decremented', () {
+//       final main = Main();
+//       main.decrement();
+//       expect(main.value, -1);
+//     });
+
+//   });
+
+
+//   test('Value needs to be incremented by 2',(){
+
+//     main.incrementPlus2();
+//     expect(main.value, 2);
+//   });
+// }
+
+
+// ##################### Test code for Widget :----
+
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test_code/main.dart';
 
 void main() {
 
-  late Main main;
+  testWidgets("FindOneWidget", (WidgetTester tester) async{
+    await tester.pumpWidget(MyApp());
 
-  setUp((){
-    main = Main();
+    var textField = find.byType(TextField);
+    expect(textField, findsOneWidget);
   });
 
-  group("Main Screen",(){
+  testWidgets("FindsNWidgets", (WidgetTester tester) async{
+    await tester.pumpWidget(MyApp());
 
-    test('Value needs to be incremented',() {
-      main.increment();
-      expect(main.value, 1);
-    });
-
-    test('Value needs to be decremented', () {
-      final main = Main();
-      main.decrement();
-      expect(main.value, -1);
-    });
-
+    var textField = find.byType(TextField);
+    expect(textField, findsNWidgets(2));
   });
 
+  testWidgets("FindsNothing", (WidgetTester tester) async{
+    await tester.pumpWidget(MyApp());
 
-  test('Value needs to be incremented by 2',(){
+    var textField = find.byType(TextButton);
+    expect(textField, findsNothing);
+  });
 
-    main.incrementPlus2();
-    expect(main.value, 2);
+  testWidgets("FindsWidgets", (WidgetTester tester) async{
+    await tester.pumpWidget(MyApp());
+
+    var textField = find.byType(TextField);
+    expect(textField, findsWidgets);
   });
 }
